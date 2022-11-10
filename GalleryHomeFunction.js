003c06e63cafe42a8412fa41d4694106acad91f3
Aiden Stephan <astephan18@georgefox.edu>
2022-11-10 14:17:54 -0800

    Added initial JavaScript to do an image search from Unsplash.

--- GalleryHomeFunction.js
+++ GalleryHomeFunction.js
@@ -1,12 +1,23 @@
 let submitBtn = document.getElementById("submitBtn");
 let inputText = document.getElementById("imgSubject");
+let image1 = document.getElementById("img1");
 
 submitBtn.addEventListener("click", async e => {
+
+   // Text box functions.
    e.preventDefault();
    console.log(inputText.value);
    inputText.value = "";
-   let url = "https://source.unsplash.com/random";
+
+   // Link/request stuff.
+   let url = "https://api.unsplash.com/search/photos?client_id=7qYCm8Ow4ntFgiF61Pqu1MncO7FSN66wtHI5CzoCqMo&page=1&query=office&>"
    let response = await fetch(url);
-   let jsonData = await response.json();
+   let jsonData = await response;
+   image1.src = jsonData.url;
+   console.log(jsonData.url);
    console.log(jsonData);
+   //console.log(jsonData.updated_at)
 });
+
+// let url = "https://source.unsplash.com/random";
+//let userUrl = "https://api.unsplash.com/photos/?client_id=7qYCm8Ow4ntFgiF61Pqu1MncO7FSN66wtHI5CzoCqMo"
