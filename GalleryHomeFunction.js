3a99b39f9c9a1e1fce13675359f9be647c24c7de
Aiden Stephan <astephan18@georgefox.edu>
2022-11-10 23:33:35 -0800

    Continued experimenting with requests. No complete success yet...

--- GalleryHomeFunction.js
+++ GalleryHomeFunction.js
@@ -12,12 +12,51 @@ submitBtn.addEventListener("click", async e => {
    // Link/request stuff.
    let url = "https://api.unsplash.com/search/photos?client_id=7qYCm8Ow4ntFgiF61Pqu1MncO7FSN66wtHI5CzoCqMo&page=1&query=office&>"
    let response = await fetch(url);
-   let jsonData = await response;
-   image1.src = jsonData.url;
-   console.log(jsonData.url);
+   let jsonData = await response.json();
+
+   // console.log(jsonData.url);
    console.log(jsonData);
-   //console.log(jsonData.updated_at)
+   console.log("here is the data: " + jsonData);
+
+
+   // let request = makeHttpObject();
+   //
+   // request.open("GET", url, true);
+   // request.send(null);
+   // request.onreadystatechange = function() {
+   //    if (request.readyState === 4) {
+   //       let replyContent = JSON.parse(request.responseText);
+   //       console.log(replyContent);
+   //       getData(replyContent);
+   //    }
+   // };
 });
+
+//
+// function getData(replyData) {
+//    console.log(replyData);
+//    console.log(typeof(replyData));
+//    console.log(replyData.keys);
+// }
+//
+// function makeHttpObject() {
+//    try {
+//       return new XMLHttpRequest();
+//    }
+//    catch (error) {}
+//    try {
+//       return new ActiveXObject("Msxml2.XMLHTTP");
+//    }
+//    catch (error) {}
+//    try {
+//       return new ActiveXObject("Microsoft.XMLHTTP");
+//    }
+//    catch (error) {}
+//
+//    throw new Error("Could not create HTTP request object.");
+// }
+
+
 
 // let url = "https://source.unsplash.com/random";
 //let userUrl = "https://api.unsplash.com/photos/?client_id=7qYCm8Ow4ntFgiF61Pqu1MncO7FSN66wtHI5CzoCqMo"
