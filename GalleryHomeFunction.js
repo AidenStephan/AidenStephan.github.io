cf26c149d7c4704155c875d209b759005bf6cd47
Aiden Stephan <astephan18@georgefox.edu>
2022-11-13 15:48:08 -0800

    Everything is working (time to make it pretty)

--- GalleryHomeFunction.js
+++ GalleryHomeFunction.js
@@ -10,15 +10,21 @@
 // -------------------------------------------------- VARIABLES --------------------------------------------------------
 // Getting the HTML submit button and text input box.
 let submitBtn = document.getElementById("submitBtn");
+let updateBtn = document.getElementById("imgNumBtn");
 let inputText = document.getElementById("imgSubject");
+let imageArea = document.getElementById("imageArea");
 
 // Information to assemble request URL for Unsplash.
-let subjectQuery;
+let subjectQuery = "";
 let unsplashUrl = "https://api.unsplash.com/search/photos";
 let clientId = "7qYCm8Ow4ntFgiF61Pqu1MncO7FSN66wtHI5CzoCqMo";
 let itemsPerPage = "25"
 let pages = "4";
 
+// Variables to hold data about the images.
+let imageData;
+let prevNumImgs = 0;
+let numImgs = 5;
 
 // -------------------------------------------------- FUNCTIONS --------------------------------------------------------
 /**
@@ -66,37 +72,86 @@ function setImage(imageNum, url) {
 
    // Sets the image source to the input URL.
    htmlImg.src = url;
+   console.log(imageId);
 }
 
-function updateImages(imageData) {
+function addOrRemoveImages() {
+   console.log("prev: " + prevNumImgs);
+   console.log("new: " + numImgs);
+
+   let difference = numImgs - prevNumImgs;
+
+   console.log("difference: " + difference);
+
+   if (difference > 0) {
+      console.log("first IF");
+
+
+      for (let i = 0; i < difference; i++) {
+         let newImage = document.createElement("img");
+         console.log("THE TYPE : " + typeof(prevNumImgs));
+         newImage.id = `img${prevNumImgs + i}`;
+         // console.log(`img${imageIdNum}`);
+         newImage.alt = "random image";
+         imageArea.appendChild(newImage);
+      }
+   }
+   else if (difference < 0) {
+      console.log("second iF");
+      let difference = prevNumImgs - numImgs;
+
+      for (let i = 0; i < difference; i++) {
+         imageArea.removeChild(document.getElementById(`img${prevNumImgs - i - 1}`));
+      }
+   }
+
+   prevNumImgs = numImgs;
+}
+
+/**
+ * Takes the URLs from the image data and displays them on the page.
+ */
+function updateImages() {
    // TODO: make sure that enough images are received to display
    // TODO: make this the input amount of images to display.
-   let numImgs = 6;
-   let numReturnedImgs = pages * itemsPerPage;
+   let numReturnedImgs = 0;
+   let numReturnedPages = 0;
+
+   for (let page of imageData) {
+      numReturnedImgs += page.length;
+      if (page.length !== 0) {
+         numReturnedPages += 1;
+      }
+   }
+
    let imgUrls = [];
 
-   let htmlImgNums = Array.from(new Array(numImgs), (x, i) => i)
+   console.log("num returned: " + numReturnedImgs);
+   console.log("pages returned: " + numReturnedPages);
+
+   addOrRemoveImages();
+   console.log(imageArea.childNodes);
+
+   let htmlImgNums = Array.from(new Array(numReturnedPages), (x, i) => i)
        .sort((a, b) => 0.5 - Math.random());
 
-   let receivedImgNums = Array.from(new Array(numReturnedImgs - 1), (x, i) => i)
-       .sort((a, b) => 0.5 - Math.random());
+   for (let i = 0; i < parseInt(pages); i++) {
+      let itemsOnPage = imageData[i].length;
 
-   for (let i = 0; i < pages; i++) {
-      for (let j = 0; j < itemsPerPage; j++) {
-         imgUrls.push(imageData[i][j].urls.regular);
+      let receivedImgNums = Array.from(new Array(itemsOnPage), (x, i) => i)
+          .sort((a, b) => 0.5 - Math.random());
+      console.log(receivedImgNums);
+
+
+      for (let j = 0; j < itemsOnPage; j++) {
+         console.log("imgNum: " + receivedImgNums[j]);
+         imgUrls.push(imageData[htmlImgNums[i]][receivedImgNums[j]].urls.regular);
       }
    }
 
    for (let i = 0; i < numImgs; i++) {
       setImage(i, imgUrls[i]);
    }
-   //
-   //
-   // console.log("number array: " + htmlImgNums);
-   // console.log("results array: " + receivedImgNums);
-   // // setImage(1, imageData[1].urls.regular)
-   // console.log("number of images: " + Object.keys(imageData).length);
-
 }
 
 /**
@@ -113,26 +168,38 @@ function getInput () {
  * Event Listener for the "Submit" button;
  */
 submitBtn.addEventListener("click", (e) => {
-   // TODO: ensure different image every time.
    e.preventDefault();
    getInput();
 
-   getImages().then((receivedData) => {
-      for (let i = 0; i < receivedData.length; i++) {
-         receivedData[i] = Object.values(receivedData[i])[2];
-      }
-      // let values = Object.values(result);
-      // let imageUrl = values[2][2].urls.regular;
-      return receivedData;
-   }).then((imagesData) => {
-      updateImages(imagesData);
-   });
+   if (subjectQuery !== "") {
+      getImages().then((receivedData) => {
+         for (let i = 0; i < receivedData.length; i++) {
+            receivedData[i] = Object.values(receivedData[i])[2];
+         }
+         // let values = Object.values(result);
+         // let imageUrl = values[2][2].urls.regular;
+         return receivedData;
+      }).then((imagesData) => {
+         imageData = imagesData;
+         updateImages();
+      });
+   }
 });
 
-
+/**
+ * Event listener for the "Update" button to refresh the number of images displayed.
+ */
+updateBtn.addEventListener("click", (e) => {
+   e.preventDefault();
+   if (imageData != null) {
+      prevNumImgs = numImgs;
+      numImgs = parseInt(document.getElementById("numImgs").value);
+      updateImages();
+   }
+});
 
 // TODO: ideas: Make 'download buttons.'
-
+// TODO: fix problem with word 'Nirvana'
 
 
 
