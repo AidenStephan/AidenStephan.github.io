d473a9e091edfaad666251223943b4235aee7c6b
Aiden Stephan <astephan18@georgefox.edu>
2022-12-03 15:01:29 -0800

    Everything working!

--- FactCentralGameFunction.js
+++ FactCentralGameFunction.js
@@ -2,12 +2,16 @@
 /**
  * Provides the function for the "Useless Facts" page of my Fact Central webpage.
  *
+ * Uses Chart.js library to display game results.
+ *
  * @type {any[]} FactCentralUselessFacts.html
  * @author astephan18@georgefox.edu
  */
+
+// import Chart from "chart.js/auto";
+
 let falseFactsData;
 let trueFactsData;
-
 let factIndex = 0;
 let correctCount = 0;
 
@@ -20,7 +24,6 @@ let gameResetBtn = document.getElementById("gameResetBtn");
 let gameTrueBtn = document.getElementById("gameTrueBtn");
 let gameFalseBtn = document.getElementById("gameFalseBtn");
 let gameNextBtn = document.getElementById("gameNextBtn");
-let titleTile = document.getElementById("titleTile");
 let gameText = document.getElementsByClassName("gameText");
 // let getFactBtn = document.getElementById("getFactBtn");
 
@@ -35,10 +38,6 @@ let currentFact;
 // Getting properties
 let screenWidth = screen.width;
 let htmlRoot = document.querySelector(":root");
-
-// Animal facts API urls
-let uselessFactsUrl = "https://uselessfacts.jsph.pl/random.json?language="
-
 
 // Set page width variable in the CSS file on page load.
 htmlRoot.style.setProperty("--page-width", `${screenWidth}px`)
@@ -98,24 +97,24 @@ function getRandomFact() {
 function resetGame() {
     fadeOut(gameResetBtn, 250);
     gameResetBtn.getAnimations()[0].finished.then(() => {
-        fadeIn(gameStartBtn, 250);
+        fadeIn(gameStartBtn, 250, "block");
     });
     fadeOut(gameText[0], 250);
     fadeOut(gameText[1], 250);
     fadeOut(gameText[2], 250);
+    fadeOut(gameText[3], 250);
+    fadeOut(gameText[4], 250);
     fadeOut(gameTrueBtn, 250);
     fadeOut(gameFalseBtn, 250);
 
+    gameText[3].innerHTML = ""
+    gameText[4].innerHTML = ""
+
     gameText[0].getAnimations()[0].finished.then(() => {
         fadeOut(gameContainer, 750);
+        gameContainer.style.padding = "0";
         stretchHeight(gameContainer, gameContainer.style.height, "0px", 500);
         stretchWidth(gameContainer, gameContainer.style.width, "0px", 500);
-        gameContainer.getAnimations()[0].finished.then(() => {
-            stretchHeight(titleTile,
-                `${titleTile.offsetHeight}px`,
-                `${titleTile.offsetHeight - 150}px`,
-                750);
-        });
     });
     factIndex = 0;
     correctCount = 0;
@@ -124,24 +123,22 @@ function resetGame() {
 function startGame() {
     fadeOut(gameStartBtn, 250);
     gameStartBtn.getAnimations()[0].finished.then(() => {
-        fadeIn(gameResetBtn, 250);
+        fadeIn(gameResetBtn, 250, "block");
     });
-    stretchHeight(titleTile,
-        `${titleTile.offsetHeight}px`,
-        `${titleTile.offsetHeight + 150}px`,
-        750);
-    titleTile.getAnimations()[0].finished.then(() => {
-        fadeIn(gameContainer, 750);
-        stretchHeight(gameContainer, gameContainer.style.height, "50%", 500);
-        stretchWidth(gameContainer, gameContainer.style.width, "150%", 500);
-        gameContainer.getAnimations()[0].finished.then(() => {
-            fadeIn(gameText[0], 250);
-            fadeIn(gameText[1], 250);
-            fadeIn(gameText[2], 250);
-            fadeIn(gameTrueBtn, 250);
-            fadeIn(gameFalseBtn, 250);
-        });
+
+    fadeIn(gameContainer, 750, "block");
+    stretchHeight(gameContainer, 0, "25vh", 500);
+    stretchWidth(gameContainer, 0, "95.5%", 1000);
+    gameContainer.style.padding = "2vw";
+
+    Promise.all(gameContainer.getAnimations().map((animation) => animation.finished)).then(() => {
+        fadeIn(gameText[0], 250, "block");
+        fadeIn(gameText[1], 250, "block");
+        fadeIn(gameText[2], 250, "block");
+        fadeIn(gameTrueBtn, 250, "inline-block");
+        fadeIn(gameFalseBtn, 250, "inline-block");
     });
+
     fetchFile(falseFactsUrl).then((receivedData) => {
         falseFactsData = Object.values(receivedData).sort((a, b) => 0.5 - Math.random());
         return fetchFile(trueFactsUrl, trueFactsApiKey);
@@ -153,11 +150,20 @@ function startGame() {
 
 function displayResults() {
     factIndex = 0;
-    for (let text of gameText) {
-        fadeOut(text, 250);
-    }
+    fadeOut(gameText[0], 250);
+    fadeOut(gameText[1], 250)
     fadeOut(gameTrueBtn);
     fadeOut(gameFalseBtn);
+    fadeOut(gameNextBtn);
+
+    gameText[2].innerHTML = "Result:";
+    gameText[3].innerHTML = String(100 * correctCount/10) + "%";
+    if (correctCount < 7) {
+        gameText[4].innerHTML = "Better luck next time.";
+    }
+    else {
+        gameText[4].innerHTML = "Good work!";
+    }
 }
 
 function playGame() {
@@ -175,46 +181,52 @@ gameTrueBtn.addEventListener("click", () => {
     if (currentFact.answer === "False") {
         gameText[3].innerHTML = 'Incorrect. This is false.';
         gameText[4].innerHTML = currentFact.explanation;
-        fadeIn(gameText[3], 250);
-        fadeIn(gameText[4], 250);
+        fadeIn(gameText[3], 250, "block");
+        fadeIn(gameText[4], 250, "block");
     }
     else if (currentFact.answer === "True") {
         gameText[3].innerHTML = "True.";
         gameText[4].innerHTML = "Well done!";
-        fadeIn(gameText[3], 250);
-        fadeIn(gameText[4], 250);
+        fadeIn(gameText[3], 250, "block");
+        fadeIn(gameText[4], 250, "block");
         correctCount ++;
     }
     gameTrueBtn.disabled = true;
     gameFalseBtn.disabled = true;
-    fadeIn(gameNextBtn, 250);
+    fadeIn(gameNextBtn, 250, "inline-block");
 });
 
 gameFalseBtn.addEventListener("click", () => {
     if (currentFact.answer === "False") {
         gameText[3].innerHTML = "Correct! This is false.";
         gameText[4].innerHTML = currentFact.explanation;
-        fadeIn(gameText[3], 250);
-        fadeIn(gameText[4], 250);
+        fadeIn(gameText[3], 250, "block");
+        fadeIn(gameText[4], 250, "block");
         correctCount ++;
     }
     else if (currentFact.answer === "True") {
         gameText[3].innerHTML = "Incorrect. This is a true fact.";
-        fadeIn(gameText[3], 250);
-        fadeIn(gameText[4], 250);
+        gameText[4].innerHTML = "Better luck next time :(";
+        fadeIn(gameText[3], 250, "block");
+        fadeIn(gameText[4], 250, "block");
     }
     gameTrueBtn.disabled = true;
     gameFalseBtn.disabled = true;
-    fadeIn(gameNextBtn, 250);
+    fadeIn(gameNextBtn, 250, "inline-block");
 });
 
 gameNextBtn.addEventListener("click", () => {
-    playGame();
+    fadeOut(gameNextBtn, 250);
     gameTrueBtn.disabled = false;
     gameFalseBtn.disabled = false;
-    fadeOut(gameNextBtn, 250);
-    fadeOut(gameText[3], 250);
-    fadeOut(gameText[4], 250);
+    if (factIndex < 10) {
+        playGame();
+        fadeOut(gameText[3], 250);
+        fadeOut(gameText[4], 250);
+    }
+    else {
+        displayResults();
+    }
 });
 
 function stretchWidth(element, startWidth, endWidth, duration) {
@@ -260,8 +272,8 @@ function fadeOut(element, duration) {
     });
 }
 
-function fadeIn(element, duration) {
-    element.style.display = "block";
+function fadeIn(element, duration, displayType) {
+    element.style.display = displayType;
 
     const fadeInFrames = [
         { opacity: 0 },
